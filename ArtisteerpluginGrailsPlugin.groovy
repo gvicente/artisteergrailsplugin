f0b2794608ede931f3310f9dfd4c427af57a5c64
Per Sundberg <per.martin.sundberg@gmail.com>
2010-12-25 21:02:02 +0100

    navigation plugin dependency template generation correction

--- ArtisteerpluginGrailsPlugin.groovy
+++ ArtisteerpluginGrailsPlugin.groovy
@@ -8,7 +8,7 @@ class ArtisteerpluginGrailsPlugin {
   // the version or versions of Grails the plugin is designed for
   def grailsVersion = "1.3.4 > *"
   // the other plugins this plugin depends on
-  def dependsOn = [:]
+  def dependsOn = [:];
   // resources that are excluded from plugin packaging
   def pluginExcludes = [
           "grails-app/views/error.gsp"
